729496a62978782bca7c271e7eb9de1595331d90
Utkarsh <utkarsh62@deloitte.com>
2025-07-04 12:55:50 +0530

    added background

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -33,7 +33,7 @@ const Body = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
-            <div className={"flex-grow flex justify-center items-center"}>
+            <div className={"flex-grow flex justify-center items-center background"}>
                 <Outlet />
             </div>
             <Footer />
